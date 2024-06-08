34ff2614cf7dfd3f14f8736233399ce02166f564
Khôi Nguyên <khoinguyenmai17102005@gmail.com>
2024-06-08 17:48:44 +0000

    almost

--- public/javascripts/components/navbar-guest.js
+++ public/javascripts/components/navbar-guest.js
@@ -1,5 +1,4 @@
-// eslint-disable-next-line no-undef
-var navbarGuest = Vue.component('navbar-guest', {
+Vue.component('navbar-guest', {
     template: `
       <header>
         <a href="/guests/index.html" class="logo"><img src="../images/logo.png" alt="Logo" class="logo-image"></a>
@@ -13,11 +12,11 @@ var navbarGuest = Vue.component('navbar-guest', {
             <li><a href="#log-in">Log In</a></li>
             <li class="sign-up"><a href="#sign-up">Sign Up</a></li>
             <div class="dropdown" :class="{ show: isDropdownVisible }">
-              <button type="button" class="dropdown-toggle" @click="toggleDropdown">SA<i class="fas fa-chevron-down"></i></button>
+              <button type="button" class="dropdown-toggle" @click="toggleDropdown">{{ selectedBranch }}<i class="fas fa-chevron-down"></i></button>
               <div class="dropdown-content">
-                <a href="#">SA</a>
-                <a href="#">TAS</a>
-                <a href="#">VIC</a>
+                <a href="#" @click.prevent="selectBranch('SA')">SA</a>
+                <a href="#" @click.prevent="selectBranch('VIC')">VIC</a>
+                <a href="#" @click.prevent="selectBranch('NSW')">NSW</a>
               </div>
             </div>
           </div>
@@ -26,7 +25,10 @@ var navbarGuest = Vue.component('navbar-guest', {
     `,
     data: function () {
         return {
-            isDropdownVisible: false
+            isDropdownVisible: false,
+            selectedBranch: 'SA',
+            // events: [],
+            // updates: [],
         };
     },
     methods: {
@@ -37,10 +39,37 @@ var navbarGuest = Vue.component('navbar-guest', {
             if (!event.target.closest(".dropdown")) {
                 this.isDropdownVisible = false;
             }
-        }
+        },
+        selectBranch(branch) {
+          this.selectedBranch = branch;
+          // this.fetchUpdatesAndEvents(branch);
+          this.toggleDropdown();
+        },
+      //   fetchUpdatesAndEvents(branch) {
+      //     const eventsXhr = new XMLHttpRequest();
+      //     eventsXhr.open('GET', `/getPublicEvents?branch=${branch}`, true);
+      //     eventsXhr.onreadystatechange = () => {
+      //       if (eventsXhr.readyState === 4 && eventsXhr.status === 200) {
+      //         this.eventsData = JSON.parse(eventsXhr.responseText);
+      //         console.log("Events data:", eventsData);
+      //       }
+      //     };
+      //     eventsXhr.send();
+
+      //     const updatesXhr = new XMLHttpRequest();
+      //     updatesXhr.open('GET', `/getPublicUpdates?branch=${branch}`, true);
+      //     updatesXhr.onreadystatechange = () => {
+      //       if (updatesXhr.readyState === 4 && updatesXhr.status === 200) {
+      //         this.updates = JSON.parse(updatesXhr.responseText);
+      //         // console.log("Events data:", updatesData);
+      //       }
+      //     };
+      //     updatesXhr.send();
+      // },
     },
     mounted() {
         document.addEventListener("click", this.closeDropdowns);
+        // this.fetchUpdatesAndEvents(this.selectedBranch);
     },
     beforeDestroy() {
         document.removeEventListener("click", this.closeDropdowns);
