08cf508ca380e882cbaa6dae9dc6e16a4c398368
Viet Bach Tran <a1901793@adelaide.edu.au>
2024-06-08 02:26:04 +0000

    fix component

--- public/javascripts/components/navbar-guest.js
+++ public/javascripts/components/navbar-guest.js
@@ -1,27 +1,48 @@
 // eslint-disable-next-line no-undef
-var navbarGuest = Vue.component(
-    'navbar-guest', {
-    template: `<header>
-      <a href="index.html" class="logo"><img src="../images/logo.png" alt="Logo" class="logo-image"></a>
-      <ul>
-        <div class="section">
-          <li><a href="guests/about.html">About Us</a></li>
-          <li><a href="guests/updates.html">Updates</a></li>
-          <li><a href="guests/events.html">Events</a></li>
-        </div>
-        <div class="section">
-          <li><a href="#log-in">Log In</a></li>
-          <li class="sign-up"><a href="#sign-up">Sign Up</a></li>
-          <div class="dropdown">
-            <button type="button" class="dropdown-toggle">SA<i class="fas fa-chevron-down"></i></button>
-            <div class="dropdown-content">
-              <a href="#">SA</a>
-              <a href="#">TAS</a>
-              <a href="#">VIC</a>
+var navbarGuest = Vue.component('navbar-guest', {
+    template: `
+      <header>
+        <a href="index.html" class="logo"><img src="../images/logo.png" alt="Logo" class="logo-image"></a>
+        <ul>
+          <div class="section">
+            <li><a href="about.html">About Us</a></li>
+            <li><a href="updates.html">Updates</a></li>
+            <li><a href="events.html">Events</a></li>
+          </div>
+          <div class="section">
+            <li><a href="#log-in">Log In</a></li>
+            <li class="sign-up"><a href="#sign-up">Sign Up</a></li>
+            <div class="dropdown" :class="{ show: isDropdownVisible }">
+              <button type="button" class="dropdown-toggle" @click="toggleDropdown">SA<i class="fas fa-chevron-down"></i></button>
+              <div class="dropdown-content">
+                <a href="#">SA</a>
+                <a href="#">TAS</a>
+                <a href="#">VIC</a>
+              </div>
             </div>
           </div>
-        </div>
-      </ul>
-    </header>`
-}
-);
+        </ul>
+      </header>
+    `,
+    data: function () {
+        return {
+            isDropdownVisible: false
+        };
+    },
+    methods: {
+        toggleDropdown() {
+            this.isDropdownVisible = !this.isDropdownVisible;
+        },
+        closeDropdowns(event) {
+            if (!event.target.closest(".dropdown")) {
+                this.isDropdownVisible = false;
+            }
+        }
+    },
+    mounted() {
+        document.addEventListener("click", this.closeDropdowns);
+    },
+    beforeDestroy() {
+        document.removeEventListener("click", this.closeDropdowns);
+    }
+});
