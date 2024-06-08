aaa8746c18add05e29527ea742f9014c32a9c869
Viet Bach Tran <a1901793@adelaide.edu.au>
2024-06-08 02:42:40 +0000

    fix bug

--- public/javascripts/components/navbar-guest.js
+++ public/javascripts/components/navbar-guest.js
@@ -2,12 +2,12 @@
 var navbarGuest = Vue.component('navbar-guest', {
     template: `
       <header>
-        <a href="index.html" class="logo"><img src="../images/logo.png" alt="Logo" class="logo-image"></a>
+        <a href="/guests/index.html" class="logo"><img src="../images/logo.png" alt="Logo" class="logo-image"></a>
         <ul>
           <div class="section">
-            <li><a href="guests/about.html">About Us</a></li>
-            <li><a href="guests/updates.html">Updates</a></li>
-            <li><a href="guests/events.html">Events</a></li>
+            <li><a href="/guests/about.html">About Us</a></li>
+            <li><a href="/guests/updates.html">Updates</a></li>
+            <li><a href="/guests/events.html">Events</a></li>
           </div>
           <div class="section">
             <li><a href="#log-in">Log In</a></li>
