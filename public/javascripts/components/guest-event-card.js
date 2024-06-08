f4e3cce10cd2fd05487b945099341731661c90d7
Viet Bach Tran <a1901793@adelaide.edu.au>
2024-06-08 02:39:40 +0000

    update guests pages

---
+++ public/javascripts/components/guest-event-card.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-undef
+var guestEventCard = Vue.component('guest-event-card', {
+    props: ['eventName', 'eventDescription', 'buttonText', 'imageUrl'],
+    template: `
+        <div class="latest-events-card">
+            <img :src="imageUrl" alt="Description of the image">
+            <div>
+                <h2>{{ eventName }}</h2>
+                <p>{{ eventDescription }}</p>
+                <div><button type="button" class="green-trans-button">{{ buttonText }}</button></div>
+            </div>
+        </div>
+    `
+});
