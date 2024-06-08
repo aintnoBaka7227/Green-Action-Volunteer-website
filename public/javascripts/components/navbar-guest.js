8c799e04f2fd2023083775d8efa51cfe42ec38be
Viet Bach Tran <a1901793@adelaide.edu.au>
2024-06-08 01:31:28 +0000

    navbar components

---
+++ public/javascripts/components/navbar-guest.js
@@ -0,0 +1,27 @@
+var navbarGuest = Vue.component(
+    'navbar-guest', {
+    template: `<header>
+        <a href="index.html" class="logo"><img src="../images/logo.png" alt="Logo" class="logo-image"></a>
+        <ul>
+            <div class="section">
+                <li><a href="about.html">About Us</a></li>
+                <li><a href="updates.html">Updates</a></li>
+                <li><a href="events.html">Events</a></li>
+            </div>
+            <div class="section">
+                <li><a href="#log-in">Log In</a></li>
+                <li class="sign-up"><a href="#sign-up">Sign Up</a></li>
+                <div class="dropdown">
+                    <button type="button" class="dropdown-toggle">SA<i class="fas fa-chevron-down"></i></button>
+                    <div class="dropdown-content">
+                        <a href="#">SA</a>
+                        <a href="#">TAS</a>
+                        <a href="#">VIC</a>
+                    </div>
+                </div>
+            </div>
+        </ul>
+    </header>`
+});
+
+export default navbarGuest;
