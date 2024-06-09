b276200118ffa5d71d8835ee83edffe8fe1d07ff
khanhgn <a1898379@adelaide.edu.au>
2024-06-09 10:50:01 +0000

    done settings

--- public/javascripts/Profile.js
+++ public/javascripts/Profile.js
@@ -15,42 +15,45 @@ const userProfileApp = new Vue({
       this.loadUserData();
   },
   methods: {
-      async loadUserData() {
-          try {
-              const response = await fetch('/getUserData');
-              if (response.ok) {
-                  const userData = await response.json();
-                  this.user.first_name = userData.first_name;
-                  this.user.last_name = userData.last_name;
-                  this.user.email = userData.email;
-                  this.user.phone_number = userData.phone_number;
-                  this.user.gender = userData.gender;
-                  this.user.dob = userData.DOB.split('T')[0];
-              } else {
-                  console.error('Error loading user data:', response.statusText);
-              }
-          } catch (error) {
-              console.error('Error loading user data:', error);
-          }
-      },
-      async handleFormSubmit() {
-          try {
-              const response = await fetch('/updateUserData', {
-                  method: 'POST',
-                  headers: {
-                      'Content-Type': 'application/json'
-                  },
-                  body: JSON.stringify(this.user)
-              });
+    async loadUserData() {
+        try {
+            const response = await fetch('/getUserData');
+            if (response.ok) {
+                const userData = await response.json();
+                this.user.first_name = userData.first_name;
+                this.user.last_name = userData.last_name;
+                this.user.email = userData.email;
+                this.user.phone_number = userData.phone_number;
+                this.user.gender = userData.gender;
+                this.user.dob = userData.DOB.split('T')[0];
+            } else {
+                console.error('Error loading user data:', response.statusText);
+            }
+        } catch (error) {
+            console.error('Error loading user data:', error);
+        }
+    },
+    async handleFormSubmit() {
+        try {
+            const response = await fetch('/updateUserData', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(this.user)
+            });
 
-              if (response.ok) {
-                  alert('User updated successfully');
-              } else {
-                  alert('Error updating user');
-              }
-          } catch (error) {
-              console.error('Error updating user:', error);
-          }
-      }
+            if (response.ok) {
+                alert('User updated successfully');
+            } else {
+                alert('Error updating user');
+            }
+        } catch (error) {
+            console.error('Error updating user:', error);
+        }
+    },
+    goBack() {
+        window.history.back(); // This will navigate back to the previous page
+    }
   }
 });
