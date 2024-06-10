868bc90a9067aa1c43a10d84bc76ec51f17f4539
khanhgn <a1898379@adelaide.edu.au>
2024-06-10 06:46:08 +0000

    searching updates and seeing all updates (checks need to be implemented to check user role with branch and to check if user is a volunteer for a given branch

---
+++ public/javascripts/components/volunteers/update-card.js
@@ -0,0 +1,38 @@
+// EventUpdateCard.js
+var EventUpdateCard = Vue.component('event-update-card', {
+  template: `
+    <div class="announcement-card">
+      <hr>
+      <div class="announcement-first-line">
+        <a class="announcement-title">
+          {{ title }}
+        </a>
+        <div class="announcement-event-name">{{ name }}</div>
+      </div>
+      <div class="announcement-second-line">
+        <div class="announcement-sub">
+          {{ content }}
+        </div>
+        <div class="announcement-date">{{ date }}</div>
+      </div>
+    </div>
+  `,
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    content: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    date: {
+      type: String,
+      required: true
+    }
+  }
+});
--- routes/volunteers.js
+++ routes/volunteers.js
@@ -9,5 +9,6 @@ router.get('/', function(req, res, next) {
 });
 
 
+
 module.exports = router;
 
